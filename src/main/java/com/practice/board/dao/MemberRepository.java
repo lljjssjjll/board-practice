cd8a017b90c5c738ffe623f475d5db509d282cab
Junseok Lee <31983252+lljjssjjll@users.noreply.github.com>
2018-07-06 16:23:34 +0900

    회원가입 구현전 작업 완료

--- src/main/java/com/practice/board/dao/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.practice.board.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.practice.board.model.Member;
-
-public interface MemberRepository extends CrudRepository<Member, Long> {
-	Member findByEmail(String email);
-}
