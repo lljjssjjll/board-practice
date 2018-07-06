c9511b5599499a6843db83c3c56edc837a9d1a4d
Junseok Lee <31983252+lljjssjjll@users.noreply.github.com>
2018-07-06 16:23:04 +0900

    사용자 로그인 및 로그아웃 구현, 회원가입 구현 전 작업 완료

---
+++ src/main/java/com/practice/board/dao/AppRoleDao.java
@@ -0,0 +1,31 @@
+package com.practice.board.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.practice.board.entity.UserRole;
+
+@Repository
+@Transactional
+public class AppRoleDao {
+
+	@Autowired
+	private EntityManager entityManager;
+	
+	public List<String> getRoleNames(Long userId) {
+		String sql = ""
+				+ "select ur.appRole.roleName "
+				+ "from " + UserRole.class.getName() + " ur "
+				+ "where ur.appUser.userId = :userId";
+		Query query = entityManager.createQuery(sql, String.class);
+		query.setParameter("userId", userId);
+		return query.getResultList();
+	}
+	
+}
